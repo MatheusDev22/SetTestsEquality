86b89d6795cae320a0c6bc73e79b6fcabb4c6f70
MatheusDev22 <matheusbsb95@gmail.com>
2022-03-22 16:03:23 -0300

    using HasCode()

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.Objects;
+
 public class Product {
 	
 	private String name;
@@ -13,6 +15,23 @@ public class Product {
 	public String getName() {
 		return name;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
+	}
+
 	public void setName(String name){
 		this.name = name;
 	}
